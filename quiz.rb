50ef786ddf7505b697b548efa5af7ac5bbd1043f
Charlee Cordts <ckcordts@gmail.com>
2019-10-09 10:54:51 -0600

    quiz modifications

--- quiz.rb
+++ quiz.rb
@@ -8,39 +8,52 @@ def three_even(list)
         end
         return false
     end
-
-# ef bigger_two(list1, list2)
-
-
-
-
+# puts three_even([2,1,3,5])#f
+# puts three_even ([2,4,12,5]) #t
+# puts three_even([2,1,4,6]) #f
+# puts three_even([1,4,6,4]) #t
+# puts three_even([]) #f
+# put in all puts satements to show code works for all senarios
+#small change made I used previous code as an example allowing me to set a foundation to this code
 
  def bigger_two(list_1,list_2)
     puts "running code"
-    x = list_1[0] + list_1[1]
-    y = list_2[0] + list_2[1]
-    if x > y
+    sum_one = list_1[0] + list_1[1]
+    sum_two = list_2[0] + list_2[1]
+    #changed variable name to sum because thats more clear than x and y
+    if sum_one > sum_two
         return list_1
-    end
-    
-    if y > x
+    else #used else to condense code
         return list_2
     end
-    
-    if y = x
+    if sum_two = sum_one
         return list_1
     end
 end
-#puts bigger_two([1,2], [3,4])
-# puts bigger_two([1, 7], [4, 4]) #[1, 7]
+#print bigger_two([1 , 2], [3, 4]) #[3 , 4] #used print to better output results
+# print bigger_two([1 , 7], [4, 4]) #[1 , 7]
 
 
-def series_up(list
-    #did not finish)
+ def series_up(num)
+    series = 0
+    list = [] #starts as empty
+    #puts "running code"
+    num.times do |pattern|
+        n = 0#original num
+        num.times do |num|
+            list[n + series] = n + 1
+            n += 1
+        end 
+        series += pattern + 1 
+        #makes it grow exponentially
+    end
+    return list 
+end 
 
-end series_up(1) #q 
-    
-
+#print series_up(1) #1
+#print series_up(2) #1,1,2 >> 1212
+#print series_up(3) #1,1,2,1,2,3
+#print series_up(4) #1,1,2,1,2,3,1,2,3,4
 
 
 
